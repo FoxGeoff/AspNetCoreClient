5bd0a1e95cdb36726863b0d438c90e0f1f131388
FoxGeoff <fox.geoffrey@gmail.com>
2018-11-08 15:02:53 -0800

    Add in-memeory-service

--- README.md
+++ README.md
@@ -33,3 +33,4 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 * Run: ```npm install angular-in-memory-web-api --save```
 * Run: ```ng g service shared/InMemoryData ```
 * Run: ```ng g interface interface/blog ```
+* Code: in-memory-data.sevice
--- src/app/shared/in-memory-data.service.ts
+++ src/app/shared/in-memory-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { Blog } from './in-memory-data.service';
+import { Blog } from './../interface/blog';
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +11,16 @@ export class InMemoryDataService implements InMemoryDbService {
 
   createDb() {
     const blogs: Blog[] = [
-      { BlogId = 11, Url = "http://sample11.com" },
-      { BlogId = 12, Url = "http://sample12.com" },
-      { BlogId = 13, Url = "http://sample13.com" },
-      { BlogId = 14, Url = "http://sample14.com" },
-      { BlogId = 15, Url = "http://sample15.com" },
-      { BlogId = 16, Url = "http://sample16.com" },
-      { BlogId = 17, Url = "http://sample17.com" },
-      { BlogId = 18, Url = "http://sample18.com" },
-      { BlogId = 19, Url = "http://sample19.com" },
-      { BlogId = 20, Url = "http://sample20.com" },
+      { BlogId : 11, Url : "http://sample11.com" },
+      { BlogId : 12, Url : "http://sample12.com" },
+      { BlogId : 13, Url : "http://sample13.com" },
+      { BlogId : 14, Url : "http://sample14.com" },
+      { BlogId : 15, Url : "http://sample15.com" },
+      { BlogId : 16, Url : "http://sample16.com" },
+      { BlogId : 17, Url : "http://sample17.com" },
+      { BlogId : 18, Url : "http://sample18.com" },
+      { BlogId : 19, Url : "http://sample19.com" },
+      { BlogId : 20, Url : "http://sample20.com" },
     ];
     return { blogs };
   }
